4342d38a6d8c95ee5becf728fcc329ab03adb9dc
Shadi Akiki <shadiakiki1986@gmail.com>
2017-02-12 12:11:51 +0000

    finished factoring out matcher

--- app/polls/matcher.py
+++ app/polls/matcher.py
@@ -7,43 +7,148 @@ from zipline.finance.blotter import Blotter
 #slippage_func = FixedSlippage(spread=0.0)
 from zipline.finance.slippage import VolumeShareSlippage
 
-from zipline.finance.execution import (
-    MarketOrder,
-)
+# try to use a data portal
+from zipline.data.data_portal import DataPortal
+from zipline._protocol import BarData
+
+# fills2reader
+from datetime import datetime, timedelta
+from six import iteritems
+import os
+#from zipline.data.us_equity_pricing import BcolzDailyBarReader, BcolzDailyBarWriter
+from zipline.data.minute_bars import BcolzMinuteBarReader, BcolzMinuteBarWriter
+from functools import reduce
+
+# constructor
+from zipline.utils.calendars import get_calendar
+from zipline.finance.trading import TradingEnvironment
+import zipline.utils.factory as factory
 
 class Matcher:
-  def orders2blotter(self, sim_params, env, orders):
+  def __init__(self):
+    self.env = TradingEnvironment()
+    
+    # save an asset
+    df = pd.DataFrame(
+        {  
+          "sid":1,
+          "exchange":'exchange name',
+          "symbol":'a1',
+          "asset_name":'a1 name',
+        },
+        index=[1],
+    )
+    self.env.write_data(equities=df)
+    
+    # prepare for data portal
+    # from zipline/tests/test_finance.py#L238
+    # Note that 2013-01-05 and 2013-01-06 were Sat/Sun
+    # Also note that in UTC, NYSE starts trading at 14.30
+    # TODO tailor for FFA Dubai
+    #  start_date=pd.Timestamp('2013-12-08 9:31AM', tz='UTC'),
+    START_DATE = pd.Timestamp('2013-01-07', tz='utc')
+    END_DATE = pd.Timestamp(datetime.now(), tz='utc')
+    self.sim_params = factory.create_simulation_parameters(
+        start = START_DATE,
+        end = END_DATE,
+        data_frequency="minute"
+    )
+    
+    self.trading_calendar=get_calendar("NYSE")
+
+  def fills2minutes(self,fills):
+    minutes = [sid.index.values for _, sid in fills.items()]
+    minutes = reduce(lambda a, b: a.concatenate(b), minutes)
+    minutes = [pd.Timestamp(x, tz='utc') for x in minutes]
+    minutes.sort()
+    minutes = list(set(minutes))
+    return minutes
+
+  def fills2reader(self, tempdir, minutes, fills):
+    
+    for _,fill in fills.items():
+      fill["open"] = fill["close"]
+      fill["high"] = fill["close"]
+      fill["low"]  = fill["close"]
+
+    days = self.trading_calendar.sessions_in_range(
+      self.trading_calendar.minute_to_session_label(
+        minutes[0]
+      ),
+      self.trading_calendar.minute_to_session_label(
+        minutes[-1]
+      )
+    )
+    print("days: %s" % (days))
+  
+    #path = os.path.join(tempdir.path, "testdata.bcolz")
+    path = tempdir.path
+    writer = BcolzMinuteBarWriter(
+      rootdir=path,
+      calendar=self.trading_calendar,
+      start_session=days[0],
+      end_session=days[-1],
+      minutes_per_day=390
+    )
+    print("Writer session labels: %s" % (writer._session_labels))
+    writer.write(iteritems(fills))
+    
+    print("temp path: %s" % (path))
+    reader = BcolzMinuteBarReader(path)
+
+    # get array of timestamps
+    columns = ['open', 'high', 'low', 'close', 'volume']
+    arrays = list(map(numpy.transpose, reader.load_raw_arrays(
+      columns, minutes[0], minutes[-1], [1],
+    )))
+    print('arrays',arrays)
+
+    return reader
+
+  def orders2blotter(self, orders):
     slippage_func = VolumeShareSlippage(
       volume_limit=1,
       price_impact=0
     )
     blotter = Blotter(
-      data_frequency=sim_params.data_frequency,
-      asset_finder=env.asset_finder,
+      data_frequency=self.sim_params.data_frequency,
+      asset_finder=self.env.asset_finder,
       slippage_func=slippage_func
     )
     
-    print("-----------------------")
     print("Place orders")
-    a1=env.asset_finder.retrieve_asset(sid=1)
-    MID_DATE_0 = pd.Timestamp('2013-01-07 15:00', tz='utc')
-    blotter.set_date(MID_DATE_0)
-    print(blotter.current_dt)
-  
-    print("Order a1 +10")
-    o1=blotter.order(a1,10,MarketOrder())
-    print("Open orders: %s" % (len(blotter.open_orders[a1])))
-    
-    print("Order a1 +10 and +10")
-    o2=blotter.order(a1,10,MarketOrder())
-    o3=blotter.order(a1,10,MarketOrder())
+    orders2 = []
+    for order in orders:
+      blotter.set_date(order["dt"])
+      print(blotter.current_dt)
+      #print("Order a1 +10")
+      #o1=
+      blotter.order(order["sid"],order["amount"],order["style"])
+
     return blotter
-   
-  def match_orders_fills(self,blotter,bar_data,fills):
+
+  def blotter2bardata(self, equity_minute_reader, blotter):
+    dp = DataPortal(
+      asset_finder=self.env.asset_finder,
+      trading_calendar=self.trading_calendar,
+      first_trading_day=equity_minute_reader.first_trading_day,
+      equity_minute_reader=equity_minute_reader
+    )
+
+    def simulation_dt_func(): return blotter.current_dt
+    bd = BarData(
+      data_portal=dp,
+      simulation_dt_func=simulation_dt_func,
+      data_frequency=self.sim_params.data_frequency,
+      trading_calendar=self.trading_calendar
+    )
+
+    return bd
+
+  def match_orders_fills(self, blotter, bar_data, all_minutes, fills):
     all_closed = []
     all_txns = []
-    for _,sid in fills.items():
-      for dt in sid.index.values:
+    for dt in all_minutes:
         print("========================")
         dt = pd.Timestamp(dt, tz='utc')
         blotter.set_date(dt)
--- testZipline.py
+++ testZipline.py
@@ -1,127 +1,46 @@
 # https://github.com/quantopian/zipline/blob/3350227f44dcf36b6fe3c509dcc35fe512965183/zipline/finance/blotter.py#L123
 import pandas as pd
 from app.polls.matcher import Matcher
-
+from zipline.finance.execution import (
+    MarketOrder,
+)
 #####################################
-from zipline.finance.trading import TradingEnvironment
 from testfixtures import TempDirectory
-import zipline.utils.factory as factory
 
 with TempDirectory() as tempdir:
-  env = TradingEnvironment()
-  
-  # save an asset
-  df = pd.DataFrame(
-      {  
-        "sid":1,
-        "exchange":'exchange name',
-        "symbol":'a1',
-        "asset_name":'a1 name',
-      },
-      index=[1],
-  )
-  env.write_data(equities=df)
-  
-  a1=env.asset_finder.retrieve_asset(sid=1)
 
-  # prepare for data portal
-  # from zipline/tests/test_finance.py#L238
-  # Note that 2013-01-05 and 2013-01-06 were Sat/Sun
-  # Also note that in UTC, NYSE starts trading at 14.30
-  # TODO tailor for FFA Dubai
-  #  start_date=pd.Timestamp('2013-12-08 9:31AM', tz='UTC'),
-  START_DATE = pd.Timestamp('2013-01-07', tz='utc')
-  MID_DATE_1 = pd.Timestamp('2013-01-07 15:01', tz='utc')
-  MID_DATE_2 = pd.Timestamp('2013-01-07 15:02', tz='utc')
-  MID_DATE_3 = pd.Timestamp('2013-01-07 15:03', tz='utc')
-  END_DATE = pd.Timestamp('2013-12-31', tz='utc')
-  sim_params = factory.create_simulation_parameters(
-      start = START_DATE,
-      end = END_DATE,
-      data_frequency="minute"
-  )
-  
-  from zipline.utils.calendars import get_calendar
-  trading_calendar=get_calendar("NYSE")
-
-  from datetime import datetime, timedelta
-  #minutes = sim_params.sessions
-  #minutes = [pd.Timestamp('2013-01-05 9:01AM', tz='utc')]
-  minutes = trading_calendar.minutes_window(
-      sim_params.first_open,
-      int((timedelta(minutes=1).total_seconds() / 60) * 1)
-      + 100)
-  print("Minutes: %s" % (minutes))
+  matcher = Matcher()
+  a1=matcher.env.asset_finder.retrieve_asset(sid=1)
  
   # Use same sid as for assets above
   # NOT Multiplying by 1000 as documented in zipline/data/minute_bars.py#L419
+  MID_DATE_1 = pd.Timestamp('2013-01-07 15:01', tz='utc')
+  MID_DATE_2 = pd.Timestamp('2013-01-07 15:02', tz='utc')
+  MID_DATE_3 = pd.Timestamp('2013-01-07 15:03', tz='utc')
   fills = {
       1: pd.DataFrame({
-  	"open": [3.5, 4.5, 4],
-  	"high": [3.5, 4.5, 4],
-  	"low": [3.5, 4.5, 4],
   	"close": [3.5, 4.5, 4],
   	"volume": [10, 5, 7],
-  	"dt": [MID_DATE_1, MID_DATE_2, MID_DATE_3]
+  	"dt": [MID_DATE_1,MID_DATE_2,MID_DATE_3]
       }).set_index("dt")
   }
   print("data: %s" % (fills))
-  
-  import os
-  #from zipline.data.us_equity_pricing import BcolzDailyBarReader, BcolzDailyBarWriter
-  from zipline.data.minute_bars import BcolzMinuteBarReader, BcolzMinuteBarWriter
-  
-  days = trading_calendar.sessions_in_range(
-    trading_calendar.minute_to_session_label(
-      minutes[0]
-    ),
-    trading_calendar.minute_to_session_label(
-      minutes[-1]
-    )
-  )
-  print("days: %s" % (days))
 
-  #path = os.path.join(tempdir.path, "testdata.bcolz")
-  path = tempdir.path
-  writer = BcolzMinuteBarWriter(
-    rootdir=path,
-    calendar=trading_calendar,
-    start_session=days[0],
-    end_session=days[-1],
-    minutes_per_day=390
-  )
-  print("Writer session labels: %s" % (writer._session_labels))
-  from six import iteritems
-  writer.write(iteritems(fills))
-  
-  print("temp path: %s" % (path))
-  equity_minute_reader = BcolzMinuteBarReader(path)
-  
-  # try to use a data portal
-  from zipline.data.data_portal import DataPortal
-  dp = DataPortal(
-    asset_finder=env.asset_finder,
-    trading_calendar=trading_calendar,
-    first_trading_day=equity_minute_reader.first_trading_day,
-    equity_minute_reader=equity_minute_reader
-  )
-  
-  orders = []
+  all_minutes = matcher.fills2minutes(fills)
+  equity_minute_reader = matcher.fills2reader(tempdir, all_minutes, fills)
+ 
+  MID_DATE_0 = pd.Timestamp('2013-01-07 15:00', tz='utc')
+  orders = [
+    {"dt": MID_DATE_0, "sid": a1, "amount": 10, "style": MarketOrder()},
+    {"dt": MID_DATE_0, "sid": a1, "amount": 10, "style": MarketOrder()},
+    {"dt": MID_DATE_0, "sid": a1, "amount": 10, "style": MarketOrder()},
+  ]
 
-  matcher = Matcher()
-  blotter = matcher.orders2blotter(sim_params, env, orders)
+  blotter = matcher.orders2blotter(orders)
   print("Open orders: %s" % (len(blotter.open_orders[a1])))
   
-  from zipline._protocol import BarData
-  def simulation_dt_func(): return blotter.current_dt
-  bd = BarData(
-    data_portal=dp,
-    simulation_dt_func=simulation_dt_func,
-    data_frequency=sim_params.data_frequency,
-    trading_calendar=trading_calendar
-  )
-
-  all_closed, all_txns = matcher.match_orders_fills(blotter, bd, fills)
+  bd = matcher.blotter2bardata(equity_minute_reader, blotter)
+  all_closed, all_txns = matcher.match_orders_fills(blotter, bd, all_minutes, fills)
 
   print("========================")
   print("Remaining open")
