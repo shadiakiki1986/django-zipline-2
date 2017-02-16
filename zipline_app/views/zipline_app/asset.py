955bd3d7ed5ebb6f7263f058432984d94be0ec31
Shadi Akiki <shadiakiki1986@gmail.com>
2017-02-16 15:05:13 +0000

    split templates

--- zipline_app/views/zipline_app/asset.py
+++ zipline_app/views/zipline_app/asset.py
@@ -6,12 +6,13 @@ from ...models.zipline_app.zipline_app import Asset
 class AssetCreate(generic.CreateView):
   model = Asset
   fields = ['asset_symbol','asset_name','asset_exchange']
+  template_name = 'zipline_app/asset/asset_form.html'
 
 # inheriting from create+get_context with asset_list instead of inheriting from listview
 # so that I can have the inline in create
 # http://stackoverflow.com/a/12883683/4126114
 class AssetList(AssetCreate):
-  template_name = 'zipline_app/asset_list.html'
+  template_name = 'zipline_app/asset/asset_list.html'
   def get_context_data(self, *args, **kwargs):
     context = super(AssetList, self).get_context_data(*args, **kwargs)
     context["asset_list"] = Asset.objects.all()
@@ -20,4 +21,4 @@ class AssetList(AssetCreate):
 class AssetDelete(generic.DeleteView):
     model = Asset
     success_url = reverse_lazy('zipline_app:assets-list')
-
+    template_name = 'zipline_app/asset/asset_confirm_delete.html'
