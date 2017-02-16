e1ae0d993b26896ac1536fe6b7184dbc43f2188e
Shadi Akiki <shadiakiki1986@gmail.com>
2017-02-16 08:17:39 +0000

    serve at root

--- zipline_project/urls.py
+++ zipline_project/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^zipline_app/', include('zipline_app.urls')),
+    url(r'^', include('zipline_app.urls')),
     url(r'^admin/', admin.site.urls),
 ]
