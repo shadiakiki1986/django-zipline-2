0e3f1c4289c831eb8f20801d2a34182bd6d556f7
Shadi Akiki <shadiakiki1986@gmail.com>
2017-03-21 19:33:29 +0400

    mf connection: using the cursor as iterator to avoid database lock + rename env vars

--- zipline_app/management/commands/_mfManager.py
+++ zipline_app/management/commands/_mfManager.py
@@ -2,32 +2,57 @@ from ...utils import getenv_or_fail
 import pymssql
 
 class MfManager:
-  def __init__(self, host: str=None, port: str=None, user: str=None, password: str=None):
-    self.server   = host     or getenv_or_fail("PYMSSQL_TEST_SERVER")  #or "localhost"
-    self.port     = port     or getenv_or_fail("PYMSSQL_TEST_PORT")    #or "6200"
-    self.user     = user     or getenv_or_fail("PYMSSQL_TEST_USERNAME")#or "root"
-    self.password = password or getenv_or_fail("PYMSSQL_TEST_PASSWORD")# or ""
+  def __init__(self, host: str=None, port: str=None, user: str=None, password: str=None, db:str=None):
+    self.server   = host     or getenv_or_fail("PYMSSQL_SERVER")  #or "localhost"
+    self.port     = port     or getenv_or_fail("PYMSSQL_PORT")    #or "6200"
+    self.user     = user     or getenv_or_fail("PYMSSQL_USERNAME")#or "root"
+    self.password = password or getenv_or_fail("PYMSSQL_PASSWORD")# or ""
+    self.db = db or getenv_or_fail("PYMSSQL_DB")
    
   # get MF names
   # http://pymssql.org/en/stable/pymssql_examples.html
-  def _execute(self, query:str):
-    conn = pymssql.connect(self.server, self.user, self.password, "Marketflow", port=self.port, as_dict=True, charset="latin1")
-    cursor = conn.cursor()
-    cursor.execute(query)
-    res = cursor.fetchall()
-    conn.close()
-    return res
+  def __enter__(self):
+    self.conn = pymssql.connect(self.server, self.user, self.password, self.db, port=self.port, as_dict=True, charset="latin1")
+    return self
 
-  def assets(self):
+  def _execute(self, query:str):
+    cursor = self.conn.cursor()
+    cursor.execute(query)
+    return cursor
+
+  def __exit__(self, exc_type, exc_val, exc):
+    self.conn.close()
+
+  def assetsCount(self):
+    cursor = self._execute("""
+      SELECT
+        count(*) as n
+      FROM TITRE
+    """)
+    res = cursor.fetchall()
+    return res[0]['n']
+
+  def assetsList(self):
     return self._execute("""
-      SELECT -- 21566 had non-utf8 character
+      SELECT
         TIT_COD, TIT_NOM
       FROM TITRE
     """)
 
-  def accounts(self):
+  def accountsCount(self):
+    cursor = self._execute("""
+      SELECT
+        count(*) as n
+      FROM CLIENT
+      where
+      CLI_TTU_COD=1 and CLI_CLOSED=0
+    """)
+    res = cursor.fetchall()
+    return res[0]['n']
+
+  def accountsList(self):
     return self._execute("""
-      SELECT -- top 1000
+      SELECT
         CLI_COD, CLI_NOM_PRE
       FROM CLIENT
       where
--- zipline_app/management/commands/importMarketflow.py
+++ zipline_app/management/commands/importMarketflow.py
@@ -16,10 +16,6 @@ class Command(BaseCommand):
          python manage.py importMarketflow --debug
          """
 
-  def __init__(self, *args, **kwargs):
-    super(Command, self).__init__(*args, **kwargs)
-    self.mfMan = MfManager()
-
   def add_arguments(self, parser):
     parser.add_argument('--debug', action='store_true', dest='debug', default=False, help='show higher verbosity')
 
@@ -29,46 +25,42 @@ class Command(BaseCommand):
     if options['debug']:
       logger.setLevel(logging.DEBUG)
 
-    logger.debug("MF assets .. get")
-    assets = self.mfMan.assets()
-    logger.debug("MF accounts .. get")
-    accounts = self.mfMan.accounts()
-
-    total = len(assets)
-    logger.debug("Django import assets: %s"%total)
-    counter = 0
-    progress = progressbar.ProgressBar(maxval=total).start()
-    for asset in assets:
-      counter+=1
-      if counter % 100 == 0:
-        progress.update(counter)
-
-      # get/create entity/row/case
-      #logger.debug("get or create: %s"%asset['TIT_COD'])
-      asset, created = Asset.objects.get_or_create(
-        asset_symbol=asset['TIT_COD'],
-        asset_name=asset['TIT_NOM'],
-        asset_exchange='N/A'
-      )
-      if created:
-        logger.debug("Creating new asset: %s"%asset)
-    progress.finish()
-
-
-    total = len(accounts)
-    logger.debug("Django import accounts: %s"%len(accounts))
-    counter = 0
-    progress = progressbar.ProgressBar(maxval=total).start()
-    for account in accounts:
-      counter+=1
-      if counter % 100 == 0:
-        progress.update(counter)
-
-      # get/create entity/row/case
-      account, created = Account.objects.get_or_create(
-        account_symbol=account['CLI_COD'],
-        #account_name=account['CLI_NOM_PRE'],
-      )
-      if created:
-        logger.debug("Creating new account: %s"%account)
-    progress.finish()
+    with MfManager() as mfMan:
+      total = mfMan.assetsCount()
+      logger.debug("Django import assets: %s"%total)
+      counter = 0
+      progress = progressbar.ProgressBar(maxval=total).start()
+      for asset in mfMan.assetsList():
+        counter+=1
+        if counter % 100 == 0:
+          progress.update(counter)
+  
+        # get/create entity/row/case
+        #logger.debug("get or create: %s"%asset['TIT_COD'])
+        asset, created = Asset.objects.get_or_create(
+          asset_symbol=asset['TIT_COD'],
+          asset_name=asset['TIT_NOM'],
+          asset_exchange='N/A'
+        )
+        if created:
+          logger.debug("Creating new asset: %s"%asset)
+      progress.finish()
+  
+  
+      total = mfMan.accountsCount()
+      logger.debug("Django import accounts: %s"%total)
+      counter = 0
+      progress = progressbar.ProgressBar(maxval=total).start()
+      for account in mfMan.accountsList():
+        counter+=1
+        if counter % 100 == 0:
+          progress.update(counter)
+  
+        # get/create entity/row/case
+        account, created = Account.objects.get_or_create(
+          account_symbol=account['CLI_COD'],
+          #account_name=account['CLI_NOM_PRE'],
+        )
+        if created:
+          logger.debug("Creating new account: %s"%account)
+      progress.finish()
