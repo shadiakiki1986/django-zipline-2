ad6e4a7fcedea0d9b971edb7fa62ab725a0a1524
Shadi Akiki <shadiakiki1986@gmail.com>
2017-03-25 16:49:35 +0200

    order update and fill update to use orderform and fillform for the select2 dropdown for assets/accounts

--- zipline_app/views/zipline_app/order.py
+++ zipline_app/views/zipline_app/order.py
@@ -53,7 +53,7 @@ class OrderDetailView(generic.DetailView):
 
 class OrderUpdateView(generic.UpdateView):
   model = Order
-  fields = ['pub_date', 'asset', 'order_side', 'amount_unsigned', 'account', 'order_text']
+  form_class = OrderForm
   template_name = 'zipline_app/order/order_form.html'
 
   def get_success_url(self):
